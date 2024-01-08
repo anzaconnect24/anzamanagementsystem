7d86e0fa088a3fa2530c7690ef303c46f4d67558
johnchuma <johnvchuma@gmail.com>
2024-01-08 09:53:35 +0300

    initial changes

---
+++ components/icons/auth_icon.js
@@ -0,0 +1,8 @@
+const AuthIcon = () => {
+    return ( <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-300">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+  </svg>
+   );
+}
+ 
+export default AuthIcon;
---
+++ utils/endpoint.js
@@ -0,0 +1,4 @@
+export const server_url = "http://localhost:6000";
+// export const server_url = "http://localhost:5000";
+
+
