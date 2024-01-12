14bf0a81f85cb6fa9d3d57322b5f6bc80c822517
johnchuma <johnvchuma@gmail.com>
2024-01-12 15:49:16 +0300

    solved

--- components/Breadcrumbs/Breadcrumb.js
+++ components/Breadcrumbs/Breadcrumb.js
@@ -1,10 +1,6 @@
 import Link from "next/link";
-interface BreadcrumbProps {
-  pageName: string;
-  prevPage:string,
-  prevLink:string,
-}
-const Breadcrumb = ({ pageName,prevPage,prevLink }: BreadcrumbProps) => {
+
+const Breadcrumb = ({ pageName,prevPage,prevLink }) => {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-title-md2 font-semibold text-black dark:text-white">
