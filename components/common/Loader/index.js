25900f3f2f31d8b004d9667b0d886189344d437e
johnchuma <johnvchuma@gmail.com>
2024-01-17 12:05:08 +0300

    fixing loader height

--- components/common/Loader/index.js
+++ components/common/Loader/index.js
@@ -1,6 +1,6 @@
-const Loader = () => {
+const Loader = ({height}) => {
   return (
-    <div className="flex h-125 items-center justify-center bg-white">
+    <div className={`flex ${height??"h-125"} items-center justify-center bg-white`}>
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
     </div>
   );
