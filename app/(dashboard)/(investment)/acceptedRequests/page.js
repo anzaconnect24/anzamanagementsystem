ede40f0ee153e48ac68aa48fbe6db90f65e8d3b7
johnchuma <johnvchuma@gmail.com>
2024-01-17 11:12:27 +0300

    accepted requests

--- app/(dashboard)/(investment)/acceptedRequests/page.js
+++ app/(dashboard)/(investment)/acceptedRequests/page.js
@@ -18,7 +18,7 @@ const Page = () => {
           setloading(false)
         })
   }, []);
-    return (
+    return loading?<Loader/>:(
     <div>
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="py-6 px-4 md:px-6 xl:px-7.5">
