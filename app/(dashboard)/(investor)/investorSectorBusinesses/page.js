f7756e62b21f28ca8ab1b93fbb1780ab9035811e
johnchuma <johnvchuma@gmail.com>
2024-01-22 17:02:52 +0300

    save

--- app/(dashboard)/(investor)/investorSectorBusinesses/page.js
+++ app/(dashboard)/(investor)/investorSectorBusinesses/page.js
@@ -29,9 +29,12 @@ const Page = () => {
   { applications.length <0?<NoData/>:
      <div>
 <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Business name</p>
+<div className="col-span-1 hidden items-center sm:flex">
+          <p className="font-medium">Joined</p>
+        </div>
+      
+        <div className="col-span-1 hidden items-center sm:flex">
+          <p className="font-medium">Name</p>
         </div>
       
         <div className="col-span-2 flex items-center">
@@ -53,9 +56,13 @@ const Page = () => {
           className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
           key={key}
         >
+           <div className="col-span-1 flex items-center">
+            <p className="text-sm text-black dark:text-white">
+              {timeAgo(item.createdAt)}
+            </p>
+          </div>
           
-          
-          <div className="col-span-2 flex items-center">
+          <div className="col-span-1 flex items-center">
             <p className="text-sm text-black dark:text-white">
               {item.name}
             </p>
