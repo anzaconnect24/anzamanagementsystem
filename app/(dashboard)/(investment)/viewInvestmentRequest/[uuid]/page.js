5a1b64170403dfd0bc32268dfe4b745cb11901b9
johnchuma <johnvchuma@gmail.com>
2024-01-22 15:27:11 +0300

    save

--- app/(dashboard)/(investment)/viewInvestmentRequest/[uuid]/page.js
+++ app/(dashboard)/(investment)/viewInvestmentRequest/[uuid]/page.js
@@ -4,16 +4,20 @@ import { sendInvestmentRequest, updateInvestmentRequest } from "@/app/controller
 import { useRouter } from "next/navigation";
 import Link from "next/link"
 import Loader from "@/components/common/Loader";
+import { useContext } from "react";
+import { UserContext } from "@/app/(dashboard)/layout";
 const Page = ({params}) => {
     const uuid = params.uuid;
     const router  =useRouter()
+    const {userDetails} = useContext(UserContext)
   
     return  ( <div>
         <Breadcrumb pageName={"Investment request"} prevLink={""} prevPage={"Businesses"}/>
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="py-6 px-4 md:px-6 xl:px-7.5">
    
-    <div className="flex space-x-3  pt-8">
+
+   {["Admin"].includes(userDetails.role)&&<div className="flex space-x-3  pt-8">
         <Link href="" onClick={()=>{
               updateInvestmentRequest(uuid,{status:"accepted"}).then(()=>{
                 router.back()
@@ -22,7 +26,8 @@ const Page = ({params}) => {
         <Link href={`/rejectApplicationRequest/${uuid}`} className="bg-danger py-3 rounded hover:opacity-95
          px-4 text-white">Reject application</Link>
 
-        </div>
+        </div>}
+    
       </div>
     
       
