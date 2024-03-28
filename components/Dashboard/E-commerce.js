cda87b4cbabca33691c91dac3bd4d5c8ccde3be0
John Vedastus Chuma <johnvchuma@gmail.com>
2024-03-28 12:12:19 +0300

    save

--- components/Dashboard/E-commerce.js
+++ components/Dashboard/E-commerce.js
@@ -12,6 +12,8 @@ import CardDataStats from "../CardDataStats";
 import dynamic from "next/dynamic";
 import {getDashboardData} from "@/app/controllers/dashboard_controller"
 import { UserContext } from "@/app/(dashboard)/layout";
+import Link from "next/link";
+import {checkIfProfileIsComplete} from "@/app/utils/check_profile"
 const MapOne = dynamic(() => import("../Maps/MapOne"), {
   ssr: false,
 });
@@ -20,6 +22,23 @@ const ECommerce = () => {
  const {data,userDetails} = useContext(UserContext)
   return (
     <>
+    {["Enterprenuer"].includes(userDetails.role) &&  checkIfProfileIsComplete(userDetails)==false &&<div className="py-4 px-8 bg-white mb-5">
+        <div className="flex justify-between items-center">
+      <div className="flex space-x-3 items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 animate-bounce text-warning">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
+</svg>
+
+        <div>
+        <div className="font-bold text-3xl text-warning">Please complete your profile</div>
+        <p>Your profile image is not complete, please complete your profile </p>
+        </div>
+      </div>
+        <div>
+        <Link href="/accountInformation" className="py-2 px-3 text-white rounded bg-success">Complete</Link>
+        </div>
+        </div>
+      </div>}
         {["Admin","Reviewer"].includes(userDetails.role) &&<div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-4 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
 
