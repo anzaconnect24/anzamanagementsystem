fbfc86e59845d401597a90198a58c16e6b37f9e0
johnchuma <johnvchuma@gmail.com>
2024-04-07 12:05:40 +0300

    save changes

--- app/utils/endpoint.js
+++ app/utils/endpoint.js
@@ -1,6 +1,6 @@
 
-export const server_url = "http://localhost:5000";
-// export const server_url = "http://195.35.8.142:5000";
+// export const server_url = "http://localhost:5000";
+export const server_url = "http://195.35.8.142:5000";
 
 
 
