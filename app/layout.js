fb7dd60f1fdb2766c47f9fcf8355844e8b557780
johnchuma <johnvchuma@gmail.com>
2024-04-08 02:02:46 +0300

    remove httpto https convertor

--- app/layout.js
+++ app/layout.js
@@ -27,9 +27,6 @@ export default function RootLayout({children}) {
 
   return (
     <html  lang="en">
-      <head>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
-      </head>
       <body suppressHydrationWarning={true}>
        {children}
       </body>
