c021ce0f6ec69ad5910190f524d8b11646a9f11f
johnchuma <johnvchuma@gmail.com>
2024-04-07 14:23:09 +0300

    save

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,9 @@ export default function RootLayout({children}) {
 
   return (
     <html  lang="en">
+      <head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+      </head>
       <body suppressHydrationWarning={true}>
        {children}
       </body>
