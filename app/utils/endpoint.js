5973756664262e4c0946bc8b1179f651bd36934c
johnchuma <johnvchuma@gmail.com>
2024-06-15 10:22:27 +0300

    connected to aws

--- app/utils/endpoint.js
+++ app/utils/endpoint.js
@@ -1,2 +1,2 @@
 // export const server_url = "http://localhost:5000";
-export const server_url = "https://anzamanagmentbackend.onrender.com";
+export const server_url = "https://serverapipointer.online";
