5df7e6a8755e05af401e6f08a65ef1a196312c14
davismwaisemba <davismwaisemba@gmail.com>
2024-12-12 13:48:02 +0300

    changed server url

--- app/utils/endpoint.js
+++ app/utils/endpoint.js
@@ -1,2 +1,2 @@
- export const server_url = "http://localhost:5001";
-// export const server_url = "https://serverapipointer.online";
+// export const server_url = "http://localhost:5001";
+ export const server_url = "https://api.anzaconnect.co.tz";
