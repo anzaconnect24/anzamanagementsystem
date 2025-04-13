143f754d7205b6ebdf5765eb3692640976934302
johnchuma <johnvchuma@gmail.com>
2025-04-13 08:28:29 +0300

    fix: render issues

--- components/Dashboard/E-commerce.js
+++ components/Dashboard/E-commerce.js
@@ -388,7 +388,7 @@ const ECommerce = () => {
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         {userDetails.role != "Mentor" && <ChartOne />}
         {/* {userDetails.role == "Admin" && <ChartTwo />} */}
-        {userDetails.role == "Admin" && <TanzaniaMap />}
+        {/* {userDetails.role == "Admin" && <TanzaniaMap />} */}
         {/* {userDetails.role == "Admin" && <ChartThree />} */}
       </div>
     </>
