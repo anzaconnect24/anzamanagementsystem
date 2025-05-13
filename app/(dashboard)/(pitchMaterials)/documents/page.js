53da727a92df34b7228aae90780ba849c2aeafaf
johnchuma <johnvchuma@gmail.com>
2025-05-13 11:56:47 +0300

    fix: pitch and signup

--- app/(dashboard)/(pitchMaterials)/documents/page.js
+++ app/(dashboard)/(pitchMaterials)/documents/page.js
@@ -1,73 +1,104 @@
-"use client"
+"use client";
 import { useContext, useEffect, useState } from "react";
-import {getApprovedBusinesses, getPendingBusinesses} from "@/app/controllers/business_controller"
-import {timeAgo} from "@/app/utils/time_ago"
-import Link from "next/link"
-import {getDocuments,deletePitchMaterial } from "@/app/controllers/pitch_material_controller";
+import {
+  getApprovedBusinesses,
+  getPendingBusinesses,
+} from "@/app/controllers/business_controller";
+import { timeAgo } from "@/app/utils/time_ago";
+import Link from "next/link";
+import {
+  getDocuments,
+  deletePitchMaterial,
+} from "@/app/controllers/pitch_material_controller";
 import { UserContext } from "../../layout";
 import NoData from "@/app/component/noData";
-import Image from "next/image"
+import Image from "next/image";
 import toast from "react-hot-toast";
+import Loader from "@/components/common/Loader";
 
 const Page = () => {
   const [documents, setDocuments] = useState([]);
   const [ShowOptions, setShowOptions] = useState(false);
-  const {userDetails}  = useContext(UserContext)
+  const { userDetails } = useContext(UserContext);
   const [refresh, setRefresh] = useState(0);
 
   const [loading, setloading] = useState(true);
   useEffect(() => {
-        getDocuments(1,5).then((body)=>setDocuments(body.data))
+    setloading();
+    getDocuments(1, 100).then((body) => {
+      setDocuments(body.data);
+      setloading(false);
+    });
   }, [refresh]);
-    return (
+  return loading ? (
+    <Loader />
+  ) : (
     <div>
       <div className="rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="py-6 px-4 md:px-6 xl:px-7.5 ">
-        <div className="flex justify-between">
-<h4 className="text-xl font-semibold text-black dark:text-white">
-          Documents
-        </h4>
-        {
-        ["Admin","Reviewer"].includes(userDetails.role)&&
-        <Link className="px-4 py-2 rounded bg-primary text-white hover:opacity-95" href={"/uploadPitchMaterial/document"}>Add</Link>
-        }
-      
-        </div>
-        {documents.length<1?<NoData/>: <div className="grid grid-cols-4 gap-3 ">
-           {documents.map((item,key)=>{
-        return <a key={key} className="py-8  px-5 border border-stroke cursor-pointer  flex flex-col justify-center items-center rounded hover:shadow-lg">
-            <a href={item.link} target="_blank" >
-                <Image height={1000} width={1000} alt="" className="w-16" src="/pdf.png"/>
-            </a>
-            <div className="mt-1">{item.fileName}</div>
-            {["Admin"].includes(userDetails.role)&&
-            <div className="flex space-x-2 justify-between mt-3">
-            <Link href={`/viewer/${item.uuid}`} className="text-sm font-bold ">Viewers</Link>
-            <h1 onClick={()=>{
-              deletePitchMaterial(item.uuid).then((data)=>{
-                setRefresh(refresh+1)
-                toast.success('Deleted successfully')
-                
-              })
-            }} className="text-sm  cursor-pointer font-bold text-danger">Delete</h1>
-
+        <div className="py-6 px-4 md:px-6 xl:px-7.5 ">
+          <div className="flex justify-between">
+            <h4 className="text-xl font-semibold text-black dark:text-white">
+              Documents
+            </h4>
+            {["Admin", "Reviewer"].includes(userDetails.role) && (
+              <Link
+                className="px-4 py-2 rounded bg-primary text-white hover:opacity-95"
+                href={"/uploadPitchMaterial/document"}
+              >
+                Add
+              </Link>
+            )}
           </div>
-            }
-            
-        </a>
-       })}
-</div>}
-       
+          {documents.length < 1 ? (
+            <NoData />
+          ) : (
+            <div className="grid grid-cols-1 2xl:grid-cols-4 gap-3  mt-4">
+              {documents.map((item, key) => {
+                return (
+                  <a
+                    key={key}
+                    className="py-8  px-5 border border-stroke cursor-pointer  flex flex-col justify-center items-center rounded hover:shadow-lg"
+                  >
+                    <a href={item.link} target="_blank">
+                      <Image
+                        height={1000}
+                        width={1000}
+                        alt=""
+                        className="w-16"
+                        src="/pdf.png"
+                      />
+                    </a>
+                    <div className="mt-1">{item.fileName}</div>
+                    {["Admin"].includes(userDetails.role) && (
+                      <div className="flex space-x-2 justify-between mt-3">
+                        {/* <Link
+                          href={`/viewer/${item.uuid}`}
+                          className="text-sm font-bold "
+                        >
+                          Viewers
+                        </Link> */}
+                        <h1
+                          onClick={() => {
+                            deletePitchMaterial(item.uuid).then((data) => {
+                              setRefresh(refresh + 1);
+                              toast.success("Deleted successfully");
+                            });
+                          }}
+                          className="text-sm  cursor-pointer font-bold text-danger"
+                        >
+                          Delete
+                        </h1>
+                      </div>
+                    )}
+                  </a>
+                );
+              })}
+            </div>
+          )}
+        </div>
       </div>
-     
-
-
-     
     </div>
-       
-       
-    </div>
-    );
-}
- 
+  );
+};
+
 export default Page;
--- app/(dashboard)/(pitchMaterials)/uploadPitchMaterial/[type]/page.js
+++ app/(dashboard)/(pitchMaterials)/uploadPitchMaterial/[type]/page.js
@@ -1,73 +1,121 @@
-"use client"
+"use client";
 import { uploadPitchMaterial } from "@/app/controllers/pitch_material_controller";
 import { useEffect, useState } from "react";
-import Link from "next/link"
+import Link from "next/link";
 import Loader from "@/components/common/Loader";
 import { useRouter } from "next/navigation";
 import Breadcrumb from "@/app/component/Breadcrumb";
 import Spinner from "@/components/spinner";
+import { uploadFile } from "@/app/controllers/file_upload_controller";
 
-const Page = ({params}) => {
-    const type = params.type
-    const router = useRouter()
-    const [loading, setloading] = useState(false);
-    return ( <div>
-               <Breadcrumb prevLink={``} prevPage={`${type}s`} pageName="Material upload" />
-         <div className="rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-      <form onSubmit={(e)=>{
-        e.preventDefault()
-        setloading(true)
-        const data = {
-            fileName : e.target.fileName.value,
-            file:e.target.file.files[0],
-            type,
-            description:e.target.description.value,
-        }
-        console.log(data)
-        uploadPitchMaterial(data).then((data)=>{
-           router.back()
-        })
-      }} className="py-6 px-4 md:px-6 xl:px-7.5">
-        <h4 className="text-xl font-semibold text-black dark:text-white">
-        Upload new pitch material
-        </h4>
-        <div className="grid grid-cols-2 gap-y-3 gap-x-3 mt-4">
-        <div>
+const Page = ({ params }) => {
+  const type = params.type;
+  const router = useRouter();
+  const [loading, setloading] = useState(false);
+  return (
+    <div>
+      <Breadcrumb
+        prevLink={``}
+        prevPage={`${type}s`}
+        pageName="Material upload"
+      />
+      <div className="rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setloading(true);
+            let payload = {
+              description: e.target.description.value,
+              type,
+              fileName: e.target.fileName.value,
+            };
+            if (type == "video") {
+              payload.url = e.target.url.value;
+              uploadPitchMaterial(payload).then((data) => {
+                router.back();
+              });
+            } else {
+              let formData = new FormData();
+              formData.append("file", e.target.file.files[0]);
+              uploadFile(formData).then((url) => {
+                payload.url = url;
+                uploadPitchMaterial(payload).then((data) => {
+                  router.back();
+                });
+              });
+            }
+          }}
+          className="py-6 px-4 md:px-6 xl:px-7.5"
+        >
+          <h4 className="text-xl font-semibold text-black dark:text-white">
+            Upload new pitch material
+          </h4>
+          <div className="grid grid-cols-2 gap-y-3 gap-x-3 mt-4">
+            <div>
+              <label className="mb-2.5 block font-medium text-black dark:text-white">
+                Title
+              </label>
+              <input
+                name="fileName"
+                required
+                className="form-style disabled:opacity-75"
+                placeholder="Enter material title"
+                type="text"
+              />
+            </div>
+            {type != "video" ? (
+              <div>
+                <label className="mb-2.5 block font-medium text-black dark:text-white">
+                  File
+                </label>
+                <input
+                  name="file"
+                  onChange={(e) => {}}
+                  required
+                  className="form-style disabled:opacity-75"
+                  placeholder="Enter email address"
+                  type="file"
+                />
+              </div>
+            ) : (
+              <div>
+                <label className="mb-2.5 block font-medium text-black dark:text-white">
+                  Video URL
+                </label>
+                <input
+                  name="url"
+                  placeholder="Enter video URL"
+                  required
+                  className="form-style"
+                />
+              </div>
+            )}
+          </div>
+          <div className="mt-3">
             <label className="mb-2.5 block font-medium text-black dark:text-white">
-               Title
+              Material description
             </label>
-            <input name="fileName"  required className="form-style disabled:opacity-75" placeholder="Enter material title" type="text"/>
-        </div>
-    
-        <div>
-            <label className="mb-2.5 block font-medium text-black dark:text-white">
-                File
-            </label>
-            <input name="file" onChange={(e)=>{
-                
-            }}  required className="form-style disabled:opacity-75" placeholder="Enter email address" 
-            type="file"/>
-        </div>
-        </div>
-        <div className="mt-3">
-            <label className="mb-2.5 block font-medium text-black dark:text-white">
-                Material description
-            </label>
-        <textarea name="description"  required className="form-style" placeholder="Write material description" type="file"/>
+            <textarea
+              name="description"
+              required
+              className="form-style"
+              placeholder="Write material description"
+              type="file"
+            />
+          </div>
 
-        </div>
-        
-        
-        <div className="flex">
-        <button type="submit" className="py-3 px-4 flex justify-center bg-primary cursor-pointer text-white rounded hover:opacity-95">
-           <div>{loading?<Spinner/>:"Upload"}</div>
+          <div className="flex">
+            <button
+              type="submit"
+              className="py-3 px-4 w-24 flex justify-center bg-primary cursor-pointer text-white rounded hover:opacity-95"
+            >
+              <div>{loading ? <Spinner /> : "Upload"}</div>
             </button>
-        </div>
-
-      </form>
-      
+          </div>
+        </form>
       </div>
-    </div> );
-}
+    </div>
+  );
+};
 
 export default Page;
